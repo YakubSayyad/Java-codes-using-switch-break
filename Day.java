65003854be70d56ccfb69b283cf386b46e603dc4
YakubSayyad <120464875+YakubSayyad@users.noreply.github.com>
2022-12-13 17:06:55 +0530

    Switch & break code
    detecting day using switch and break code

---
+++ Day.java
@@ -0,0 +1,30 @@
+class Day {
+    public static void main(String[] args) {
+        String day = "Friday";
+        switch (day) {
+            case "Monday":
+                System.out.println(1);
+                break;
+            case "Tuesday":
+                System.out.println(2);
+                break;
+            case "Wednesday":
+                System.out.println(3);
+                break;
+            case "Thursday":
+                System.out.println(4);
+                break;
+            case "Friday":
+                System.out.println(5);
+                break;
+            case "Saturday":
+                System.out.println(6);
+                break;
+            case "Sunday":
+                System.out.println(7);
+                break;
+            default:
+                System.out.println("please enter valid input");
+        }
+    }
+}
