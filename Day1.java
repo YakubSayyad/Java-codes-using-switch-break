e7e63989b31bbcf3d21ae902aac4cccc40f0f9ed
YakubSayyad <120464875+YakubSayyad@users.noreply.github.com>
2022-12-13 17:23:20 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,3 +1,5 @@
+---------------------------------------------------------------------------------------------------------
+                      *code:1* 
 class Day {
     public static void main(String[] args) {
         String day = "Friday";
@@ -28,3 +30,80 @@ class Day {
         }
     }
 }
+
+
+Output:-
+    5
+---------------------------------------------------------------------------------------------------------
+                      *code:2* 
+class Day {
+    public static void main(String[] args) {
+        String day = "Today";
+        switch (day) {
+            case "Monday":
+                System.out.println(1);
+                break;
+            case "Tuesday":
+                System.out.println(2);
+                break;
+            case "Wednesday":
+                System.out.println(3);
+                break;
+            case "Thursday":
+                System.out.println(4);
+                break;
+            case "Friday":
+                System.out.println(5);
+                break;
+            case "Saturday":
+                System.out.println(6);
+                break;
+            case "Sunday":
+                System.out.println(7);
+                break;
+            default:
+                System.out.println("please enter valid input");
+        }
+    }
+}
+
+
+Output:-
+    please enter valid input
+---------------------------------------------------------------------------------------------------------
+                      *code:3* 
+class Day {
+    public static void main(String[] args) {
+        String day = "Thursday";
+        switch (day) {
+            case "Monday":
+                System.out.println(1);
+                break;
+            case "Tuesday":
+                System.out.println(2);
+                break;
+            case "Wednesday":
+                System.out.println(3);
+                break;
+            case "Thursday":
+                System.out.println(4);
+                break;
+            case "Friday":
+                System.out.println(5);
+                break;
+            case "Saturday":
+                System.out.println(6);
+                break;
+            case "Sunday":
+                System.out.println(7);
+                break;
+            default:
+                System.out.println("please enter valid input");
+        }
+    }
+}
+
+
+Output:-
+    4
+------------------------------------------------------------------------------------------
